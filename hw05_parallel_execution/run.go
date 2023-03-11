474c068b62ec05365fd70a303cd2676a3c4b46af
fenych <filatkinen@gmail.com>
2023-03-11 09:50:17 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,100 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"sync"
 )
 
-var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var (
+	ErrErrorsLimitExceeded       = errors.New("errors limit exceeded")
+	ErrErrorsWorkerHasBePositive = errors.New("количество обработчиков не может быть меньше 1")
+)
 
 type Task func() error
 
+type ResultTask struct {
+	countSuccess int
+	countError   int
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+func Run(tasks []Task, n, m int) (resultFunc ResultTask, err error) {
+	if n < 1 {
+		return resultFunc, ErrErrorsWorkerHasBePositive
+	}
+	if m < 0 {
+		m = 0
+	}
+	countError := 0
+	countSuccess := 0
+	cancelChan := make(chan struct{})
+	resultChan := make(chan error, n)
+
+	// Send tasks to chan  with buffer length=min(len(tasks),n)
+	taskBufferSize := len(tasks)
+	if taskBufferSize > n {
+		taskBufferSize = n
+	}
+	countSendTask := taskBufferSize
+	taskChan := make(chan Task, taskBufferSize)
+	for i := 0; i < taskBufferSize; i++ {
+		taskChan <- tasks[i]
+	}
+
+	// Starting "n" handlers
+	wg := sync.WaitGroup{}
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case t := <-taskChan:
+					resultChan <- t()
+				case <-cancelChan:
+					return
+				}
+			}
+		}()
+	}
+
+	funcCountResult := func(result error) {
+		if result != nil {
+			if err != nil {
+				err = fmt.Errorf(" %w", result)
+			} else {
+				err = result
+			}
+			countError++
+		} else {
+			countSuccess++
+		}
+	}
+
+	for result := range resultChan {
+		funcCountResult(result)
+		tasksLeft := len(tasks) - countSendTask
+		if !(countError > m) && tasksLeft > 0 {
+			countSendTask++
+			taskChan <- tasks[countSendTask-1]
+		} else {
+			close(cancelChan)
+			break
+		}
+	}
+
+	wg.Wait()
+	close(resultChan)
+
+	for result := range resultChan {
+		funcCountResult(result)
+	}
+
+	if countError > m {
+		err = fmt.Errorf(" %w", ErrErrorsLimitExceeded)
+	}
+	resultFunc.countError = countError
+	resultFunc.countSuccess = countSuccess
+
+	return resultFunc, err
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -32,7 +32,7 @@ func TestRun(t *testing.T) {
 
 		workersCount := 10
 		maxErrorsCount := 23
-		err := Run(tasks, workersCount, maxErrorsCount)
+		_, err := Run(tasks, workersCount, maxErrorsCount)
 
 		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
@@ -60,11 +60,115 @@ func TestRun(t *testing.T) {
 		maxErrorsCount := 1
 
 		start := time.Now()
-		err := Run(tasks, workersCount, maxErrorsCount)
+		_, err := Run(tasks, workersCount, maxErrorsCount)
 		elapsedTime := time.Since(start)
 		require.NoError(t, err)
-
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+}
+
+func TestRunAdditional(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	t.Run("Check if ErrorLimit <0. So we take it as 0.  ", func(t *testing.T) {
+		sCount := 50
+		eCount := 50
+		tasksCount := sCount + eCount
+		workersCount := 10
+		maxErrorsCount := -1
+		tasks := make([]Task, 0, tasksCount)
+
+		for i := 0; i < sCount; i++ {
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				return nil
+			})
+		}
+		for i := sCount; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				return err
+			})
+		}
+		result, err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.LessOrEqual(t, result.countSuccess+result.countError, sCount+workersCount, "extra tasks were started")
+	})
+
+	t.Run("Check concurrency if sleeptime in function==0, functions=1000, workers=100", func(t *testing.T) {
+		tasksCount := 1_000
+		workersCount := 100
+		maxErrorsCount := 0
+		tasks := make([]Task, 0, tasksCount)
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				return nil
+			})
+		}
+		result, err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Nil(t, err)
+		require.Equal(t, result.countSuccess+result.countError, tasksCount)
+	})
+
+	t.Run("Checking  if workers<1", func(t *testing.T) {
+		tasksCount := 100
+		workersCount := 0
+		maxErrorsCount := 1
+		tasks := make([]Task, 0, tasksCount)
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				return nil
+			})
+		}
+		_, err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsWorkerHasBePositive), "actual err - %v", err)
+	})
+}
+
+func TestRunAdditionalTimer(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	t.Run("Checking concurrency time execution-avoid sleep CI issue  - with Ticker", func(t *testing.T) {
+		sCount := 50
+		eCount := 0
+		tasksCount := sCount + eCount
+		workersCount := 10
+		maxErrorsCount := 0
+		tasks := make([]Task, 0, tasksCount)
+		durationFunc := time.Millisecond * 100
+		durationTicker := time.Millisecond * 2
+
+		for i := 0; i < sCount; i++ {
+			tasks = append(tasks, func() error {
+				startTime := time.Now()
+				ticker := time.NewTicker(durationTicker)
+				for range ticker.C {
+					if time.Since(startTime) >= durationFunc {
+						break
+					}
+				}
+				ticker.Stop()
+				return nil
+			})
+		}
+
+		start := time.Now()
+		result, err := Run(tasks, workersCount, maxErrorsCount)
+		elapsedTime := int64(time.Since(start))
+		estimatedTime := int64(durationFunc) * int64(tasksCount) / int64(workersCount)
+		ratio := float32(elapsedTime) / float32(estimatedTime)
+
+		require.Nil(t, err)
+		require.Equal(t, result.countSuccess+result.countError, tasksCount)
+		fmt.Printf("Ratio of elapsed to estimated test time: %.2f\n", ratio)
+		require.LessOrEqual(t, ratio, float32(1.2), "tasks were run sequentially?")
 	})
 }
