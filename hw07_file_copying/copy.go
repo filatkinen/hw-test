31b0409dd9f5fd9620aa2ff904cc8ce8540b0ac9
fenych <filatkinen@gmail.com>
2023-03-31 07:18:12 +0300

    HW7 is completed#1

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+	"time"
 )
 
 var (
@@ -10,6 +13,83 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	src, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	fi, err := src.Stat()
+	if err != nil {
+		return err
+	}
+	srcsize := fi.Size()
+	if srcsize == 0 || fi.IsDir() {
+		return ErrUnsupportedFile
+	}
+	if offset >= srcsize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	dst, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if offset > 0 {
+		_, err = src.Seek(offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+
+	buf := make([]byte, 1<<15)
+
+	var barsize int64
+	switch {
+	case offset+limit > srcsize:
+		barsize = srcsize - offset
+	case limit > 0:
+		barsize = limit
+	default:
+		barsize = srcsize - offset
+	}
+	bar := NewBar(barsize)
+
+	ticker := time.NewTicker(30 * time.Millisecond)
+
+	rbytes := int64(0)
+	wbytes := int64(0)
+	for {
+		n, err := src.Read(buf)
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		rbytes += int64(n)
+		if limit != 0 && rbytes >= limit {
+			n, err = dst.Write(buf[:n-int(rbytes-limit)])
+		} else {
+			n, err = dst.Write(buf[:n])
+		}
+		if err != nil {
+			return err
+		}
+		wbytes += int64(n)
+		select {
+		case <-ticker.C:
+			bar.ShowProgress(wbytes)
+		default:
+		}
+	}
+	bar.ShowProgress(wbytes)
+	bar.Finish()
+
+	if err := src.Close(); err != nil {
+		return err
+	}
+	return dst.Close()
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,77 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const tofile = "testdata/out.txt"
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	from := "testdata/input.txt"
+	src, err := os.Open(from)
+	if err != nil {
+		t.Error("Failed to read testing file")
+	}
+	fi, _ := src.Stat()
+	ifsize := fi.Size()
+	bufin := make([]byte, ifsize)
+	bufout := make([]byte, ifsize)
+	src.Close()
+
+	for _, v := range []int64{1, 10, 100, 1000} {
+		t.Run(fmt.Sprintf("App call test with offset=%d and limit=%d", v, v), func(t *testing.T) {
+			err := Copy(from, tofile, v, v)
+			require.Nil(t, err)
+			src, err := os.Open(from)
+			if err != nil {
+				t.Errorf("Failed to open in file %s in test with offset=%d and limit=%d", tofile, v, v)
+			}
+			defer src.Close()
+			dst, err := os.Open(tofile)
+			if err != nil {
+				t.Errorf("Failed to open out file %s in test with offset=%d and limit=%d", tofile, v, v)
+			}
+			defer dst.Close()
+			src.ReadAt(bufin, v)
+			n, _ := dst.Read(bufout)
+			for i := 0; i < n; i++ {
+				require.Equal(t, bufin[i], bufout[i], "in position ", i)
+			}
+		})
+	}
+	_ = os.Remove(tofile)
+}
+
+func TestParams(t *testing.T) {
+	from := "testdata/input.txt"
+	src, err := os.Open(from)
+	if err != nil {
+		t.Error("Failed to read testing file")
+	}
+	fi, _ := src.Stat()
+	ifsize := fi.Size()
+	t.Run("offset больше, чем размер файла - невалидная ситуация", func(t *testing.T) {
+		ofs := ifsize + 100
+		err := Copy(from, tofile, ofs, 0)
+		require.Equal(t, err, ErrOffsetExceedsFileSize)
+	})
+	_ = os.Remove(tofile)
+}
+
+func TestUnsupportedFiles(t *testing.T) {
+	t.Run("Входной файл - папка: ожидаем ошибку ErrUnsupportedFile", func(t *testing.T) {
+		from := "/tmp"
+		err := Copy(from, tofile, 0, 0)
+		require.Equal(t, err, ErrUnsupportedFile)
+	})
+	t.Run("Входной файл - /dev/urandom(размер файла=0): ожидаем ошибку ErrUnsupportedFile", func(t *testing.T) {
+		from := "/dev/urandom"
+		err := Copy(from, tofile, 0, 0)
+		require.Equal(t, err, ErrUnsupportedFile)
+	})
+	_ = os.Remove(tofile)
 }
