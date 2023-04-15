0657dfdcdd0ef50edf0a512493cdbbea5a228bad
fenych <filatkinen@gmail.com>
2023-04-15 22:15:04 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"unicode"
+)
+
+var ErrOpenDirectory = errors.New("can not read directory")
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +21,59 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+func readEnvFile(fileName string) (e string, err error) {
+	fenv, err := os.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer fenv.Close()
+	scanner := bufio.NewScanner(fenv)
+	scanner.Scan()
+	b := bytes.ReplaceAll(scanner.Bytes(), []byte("\x00"), []byte("\n"))
+	return strings.TrimRightFunc(string(b), unicode.IsSpace), nil
+}
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	env := make(map[string]EnvValue, 10)
+	dirRead, err := os.Open(dir)
+	if err != nil {
+		return nil, ErrOpenDirectory
+	}
+	defer dirRead.Close()
+
+	files, err := dirRead.ReadDir(0)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		fname := filepath.Join(dir, file.Name())
+		f, err := file.Info()
+		if err != nil {
+			log.Printf("Can not get stat information of file %s, miss it", fname)
+			continue
+		}
+		if strings.Contains(f.Name(), "=") {
+			log.Printf("File %s contain in it's name =, miss it", fname)
+			continue
+		}
+
+		e := EnvValue{Value: "", NeedRemove: true}
+
+		if f.Size() != 0 {
+			e.NeedRemove = false
+			e.Value, err = readEnvFile(fname)
+			if err != nil {
+				log.Printf("Can not read or open file %s, miss it", fname)
+				continue
+			}
+			if len(e.Value) == 0 {
+				e.NeedRemove = true
+			}
+		}
+		env[f.Name()] = e
+	}
+	return env, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,51 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	command := exec.Command(cmd[0], cmd[1:]...) // #nosec G204
+	command.Env = make([]string, 0, 50)
+	envmap := make(map[string]string, 50)
+
+	command.Stdout = os.Stdout
+	command.Stdin = os.Stdin
+	command.Stderr = os.Stderr
+
+	for _, v := range os.Environ() {
+		e := strings.SplitN(v, "=", 2)
+		if len(e) == 2 {
+			envmap[e[0]] = e[1]
+		}
+	}
+	for k, v := range env {
+		if v.NeedRemove {
+			delete(envmap, k)
+		} else {
+			envmap[k] = v.Value
+		}
+	}
+	for k, v := range envmap {
+		command.Env = append(command.Env, k+"="+v)
+	}
+
+	if err := command.Start(); err != nil {
+		log.Printf("cmd.Start: %v", err)
+		return -1
+	}
+	if err := command.Wait(); err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok { //nolint:errorlint
+			returnCode = exiterr.ExitCode()
+		} else {
+			log.Printf("cmd.Wait: %v", err)
+			return -1
+		}
+	}
+
+	return returnCode
 }
