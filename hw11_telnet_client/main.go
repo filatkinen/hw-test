5d9ce697f8168c626fd6d2e01ef5e3f515d34eab
filatkinen <filatkinen@gmail.com>
2023-05-10 11:16:45 +0300

    HW11 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,110 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+type Options struct {
+	host    string
+	port    string
+	timeout time.Duration
+}
+
+func GetFlags() (Options, error) {
+	const timeout = time.Second * 10
+	var options Options
+	fl := flag.NewFlagSet("main", flag.ContinueOnError)
+	fl.DurationVar(&options.timeout, "timeout", timeout, "timeout")
+	fl.Usage = func() {
+	}
+	err := fl.Parse(os.Args[1:])
+	if err != nil || fl.NArg() < 2 {
+		fmt.Printf("Please use: %s timeout host port\n", os.Args[0])
+		fmt.Printf("Where timeout is optional and default=%s, f.e: timeout=5s  or timeout=2m\n", timeout)
+		return options, errors.New("bad parameters")
+	}
+	options.host = fl.Args()[0]
+	options.port = fl.Args()[1]
+	return options, nil
+}
+
+func receiveRoutine(tc TelnetClient) {
+	err := tc.Receive()
+	if err != nil {
+		log.Printf("Got errror during receiving: %v\n", err)
+	}
+}
+
+func sendRoutine(tc TelnetClient, cancelChan chan struct{}, wg *sync.WaitGroup, in *bytes.Buffer) {
+	defer wg.Done()
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		in.WriteString(scanner.Text() + "\n")
+		select {
+		case <-cancelChan:
+			return
+		default:
+		}
+		err := tc.Send()
+		if err != nil {
+			log.Printf("Got error: %v\n", err)
+			return
+		}
+	}
+	if scanner.Err() == nil {
+		fmt.Printf("You typed Control+D. Goodbye!\n")
+	}
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	in := new(bytes.Buffer)
+
+	options, err := GetFlags()
+	if err != nil {
+		return
+	}
+	tc := NewTelnetClient(net.JoinHostPort(options.host, options.port), options.timeout, io.NopCloser(in), os.Stdout)
+
+	err = tc.Connect()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer func() {
+		err := tc.Close()
+		if err != nil {
+			log.Printf("Got error while closing TelnetClient: %v\n", err)
+		}
+	}()
+
+	exitSignal := make(chan os.Signal, 1)
+	cancelChan := make(chan struct{})
+	signal.Notify(exitSignal, syscall.SIGINT)
+	go func() {
+		<-exitSignal
+		close(cancelChan)
+	}()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		receiveRoutine(tc)
+	}()
+
+	go func() {
+		sendRoutine(tc, cancelChan, wg, in)
+	}()
+	wg.Wait()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"io"
+	"net"
 	"time"
+)
+
+var (
+	ErrErrorsConnect = errors.New("unable to connect")
+	ErrErrorsSend    = errors.New("unable to send")
+	ErrErrorsReceive = errors.New("unable to receive")
+	ErrErrorsClose   = errors.New("error with close")
 )
 
 type TelnetClient interface {
@@ -12,10 +22,67 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type Telnet struct {
+	in      io.ReadCloser
+	out     io.Writer
+	timeout time.Duration
+	address string
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &Telnet{
+		in:      in,
+		out:     out,
+		timeout: timeout,
+		address: address,
+	}
+}
+
+func (t *Telnet) Connect() error {
+	conn, err := net.DialTimeout("tcp", t.address, t.timeout)
+	if err != nil {
+		return errors.Join(ErrErrorsConnect, err)
+	}
+	t.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *Telnet) Send() error {
+	scanner := bufio.NewScanner(t.in)
+	for scanner.Scan() {
+		_, err := t.conn.Write(append(scanner.Bytes(), '\n'))
+		if err != nil {
+			return errors.Join(ErrErrorsSend, err)
+		}
+	}
+	if scanner.Err() != nil {
+		return errors.Join(ErrErrorsSend, scanner.Err())
+	}
+	return nil
+}
+
+func (t *Telnet) Receive() error {
+	scanner := bufio.NewScanner(t.conn)
+	for scanner.Scan() {
+		_, err := t.out.Write(append(scanner.Bytes(), '\n'))
+		if err != nil {
+			return errors.Join(ErrErrorsReceive, err)
+		}
+	}
+	if scanner.Err() != nil {
+		return errors.Join(ErrErrorsReceive, scanner.Err())
+	}
+	return nil
+}
+
+func (t *Telnet) Close() error {
+	if t.conn == nil {
+		return errors.Join(ErrErrorsClose, errors.New("connection is already closed"))
+	}
+	err := t.conn.Close()
+	if err != nil {
+		return errors.Join(ErrErrorsClose, err)
+	}
+	return nil
+}
